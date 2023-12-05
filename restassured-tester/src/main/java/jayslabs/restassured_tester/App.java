d46d0bbae7bec2b533412971aec9ae249824fa6f
jay menorca <zaimenorca@gmail.com>
2023-12-04 21:40:48 -0500

    init commit from eclipse

---
+++ restassured-tester/src/main/java/jayslabs/restassured_tester/App.java
@@ -0,0 +1,13 @@
+package jayslabs.restassured_tester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ restassured-tester/src/main/java/jayslabs/restassured_tester/Test1.java
@@ -0,0 +1,14 @@
+package jayslabs.restassured_tester;
+
+public class Test1 {
+
+	public Test1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
