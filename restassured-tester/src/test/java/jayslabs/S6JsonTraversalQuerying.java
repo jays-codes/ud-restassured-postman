84343d6dcaa3f2726246aab168fce3eb46838914
jay menorca <zaimenorca@gmail.com>
2023-12-12 09:09:03 -0500

    30;added test

--- restassured-tester/src/test/java/jayslabs/S6JsonTraversalQuerying.java
+++ restassured-tester/src/test/java/jayslabs/S6JsonTraversalQuerying.java
@@ -3,6 +3,7 @@ package jayslabs;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.path.json.JsonPath;
@@ -25,5 +26,25 @@ public class S6JsonTraversalQuerying {
 			System.out.println(jspath.get("courses["+ i +"].title") + ":" +
 					jspath.get("courses[" + i + "].price"));
 		}
+	}
+	
+	@Test
+	public void testSumOfCourses() {
+		JsonPath jspath = new JsonPath(Payload.getMockBody1());
+		
+		int courseCnt = jspath.getInt("courses.size()");
+		
+		int exptotal = jspath.getInt("dashboard.purchaseAmount");
+		int actual = 0;
+		
+		for (int i=0;i<courseCnt;i++) {
+			actual = actual + (jspath.getInt("courses[" + i + "].price") * 
+					jspath.getInt("courses[" + i + "].copies"));
+		}
+
+		System.out.println("expected: " + exptotal);
+		System.out.println("actual: " + actual);
+
+		Assert.assertEquals(actual, exptotal);
 	}
 }
