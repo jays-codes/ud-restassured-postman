675c5535e9b4e0702ec429fb769bea53b286b413
jay menorca <zaimenorca@gmail.com>
2024-01-02 15:36:12 -0500

    45; using SessionFilter to pass in session info to subsequent requests

--- restassured-tester/src/test/java/jayslabs/JiraAPITester.java
+++ restassured-tester/src/test/java/jayslabs/JiraAPITester.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
+import io.restassured.filter.session.SessionFilter;
 
 
 public class JiraAPITester {
@@ -17,30 +18,70 @@ public class JiraAPITester {
 	 * 4. Get issue details and verify if added comment and attachment exists via Get Issue API
 	 * */
 	
-	public void login() {
-		
-	}
-	
 	@Test
-	public void addComment() {
+	public void login() {
+		SessionFilter sfilter = new SessionFilter();
+		
 		RestAssured.baseURI = "http://localhost:8090";
 		
 		String resp = 
 		given()
+			.header("Content-Type", "application/json")
+			.body("{\r\n"
+					+ "    \"username\": \"zaimenorca\",\r\n"
+					+ "    \"password\": \"Kamusta@123\"\r\n"
+					+ "}")
+			.log().all()
+			.filter(sfilter)
+		.when()
+			.post("/rest/auth/1/session")
+		.then()
+			.log().all().assertThat().statusCode(200).extract().response()
+			.asString();
+		
+		//JiraAPITester.sessFilter = sfilter;
+	}
+	
+	@Test
+	public void addComment() {
+		
+		RestAssured.baseURI = "http://localhost:8090";
+
+		SessionFilter sessFilter = new SessionFilter();
+
+		
+		String resp = 
+				given()
+					.header("Content-Type", "application/json")
+					.body("{\r\n"
+							+ "    \"username\": \"zaimenorca\",\r\n"
+							+ "    \"password\": \"Kamusta@123\"\r\n"
+							+ "}")
+					.log().all().filter(sessFilter)
+				.when()
+					.post("/rest/auth/1/session")
+				.then()
+					.log().all().assertThat().statusCode(200).extract().response()
+					.asString();
+		
+		//10004 is issue id
+		resp = 
+		given()
 			.log().all()
 			.header("Content-Type", "application/json")
-			.header("Cookie","JSESSIONID=C10F42164CD90D15D350C3F41014D64E;")
+			//.header("Cookie","JSESSIONID=8D692F02B042B27B6980B8D3C0BB3093;")
 			.pathParam("id", "10004").body("{\r\n"
 					+ "    \"update\": {\r\n"
 					+ "        \"comment\": [\r\n"
 					+ "            {\r\n"
 					+ "                \"add\": {\r\n"
-					+ "                    \"body\": \"hoi hoi hoi. updated cookie!!!\"\r\n"
+					+ "                    \"body\": \"hoi hoi hoi. #2 updated cookie 2-jan-2024 !!!\"\r\n"
 					+ "                }\r\n"
 					+ "            }\r\n"
 					+ "        ]\r\n"
 					+ "    }\r\n"
 					+ "}")
+		.filter(sessFilter)	
 		.when()
 			.put("/rest/api/2/issue/{id}")
 		.then()
