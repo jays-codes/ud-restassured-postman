8c1b38be272d23e15a6a76ccdb5064420e4b078b
jay menorca <zaimenorca@gmail.com>
2024-01-08 15:21:23 -0500

    86; initial project commit

---
+++ restassured-framework-jaydemo/src/main/java/jayslabs/restassured_framework_jaydemo/App.java
@@ -0,0 +1,13 @@
+package jayslabs.restassured_framework_jaydemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
